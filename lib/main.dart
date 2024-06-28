4e6eaa128950c0b857ed3f26c9a8fa3c67d1a20a
Kauã Muniz <104865007+kauamunizz@users.noreply.github.com>
2024-06-27 21:21:49 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(new PerguntaApp());
-}
+main() =>  runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget{
 
+  void responder() {
+    print("Pergunta respondida");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final perguntas = [
+      "Qual é minha cor favorita?",
+      "Qual o meu animal favorito?"
+    ];
+
     return MaterialApp(
-      home: Text("Olá Mundo"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Perguntas"),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+            ElevatedButton(
+              child: Text("Resposta 1"),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text("Resposta 2"),
+              onPressed: responder,
+            ),
+            ElevatedButton(
+              child: Text("Resposta 3"),
+              onPressed: responder,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
